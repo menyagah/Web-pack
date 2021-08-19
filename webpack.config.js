c69897d5886ebbefa97d094ed5ac9f2dfd45a52e
menyagah <menyagah27@gmail.com>
2021-08-19 15:32:41 +0300

    Change the contentHash to contenthash

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.[contentHash].js",
+        filename: "main.[contenthash].js",
         path: path.resolve(__dirname, "dist")
     },
 
